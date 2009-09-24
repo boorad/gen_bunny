0ffdb61b553efdd257d6946f747eb8667dddcb4a
Andy Gross <andy@basho.com>
2009-09-24 00:48:25 -0400

    add edocs and -spec annotations

--- src/bunny_util.erl
+++ src/bunny_util.erl
@@ -1,5 +1,31 @@
--module(bunny_util).
+%% The MIT License
 
+%% Copyright (c) David Reid <dreid@dreid.org>, Andy Gross <andy@andygross.org>
+
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+
+%% The above copyright notice and this permission notice shall be included in
+%% all copies or substantial portions of the Software.
+
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%% THE SOFTWARE.
+
+%% @doc Utility functions for creating and manipulating RabbitMQ queues,
+%%      exchanges, and messages.
+
+-module(bunny_util).
+-author('David Reid <dreid@dreid.org>').
+-author('Andy Gross <andy@andygross.org>').
 -include("gen_bunny.hrl").
 
 -export([new_message/1,
@@ -13,32 +39,68 @@
 
 -export([new_binding/3]).
 
+-export([new_exchange/1,
+         new_exchange/2,
+         get_type/1,
+         set_type/2]).
+
 -export([is_durable/1,
          set_durable/2]).
+
+%% @type message()=#content{}
+%% @type payload()=#binary{}
+%% @type delivery_mode()=non_neg_integer()
+%% @type content_type()=binary()
+%% @type exchange()=#'exchange.declare'{}
+%% @type servobj_name()=binary()
+%% @type exchange_type()=binary()
+%% @type bunny_queue()=#'queue.declare'{}
+%% @type binding()=#binding{}
+%% @type durable_obj()=exchange()|bunny_queue()
+
 
 %%
 %% Message helpers
 %%
 
+%% @spec new_message(Payload::payload()) -> message()
+%% @doc Construct a new message with a binary Payload.
+-spec(new_message(payload()) -> message()).
 new_message(Payload) when is_binary(Payload) ->
     #content{
              class_id=60,
              properties= #'P_basic'{},
              payload_fragments_rev=[Payload]}.
 
+%% @spec get_payload(Message::message()) -> payload()
+%% @doc Return the payload for a message.
+-spec(get_payload(message()) -> payload()).
 get_payload(#content{payload_fragments_rev=[Payload]}) ->
     Payload.
 
+%% @spec get_delivery_mode(Message::message()) -> delivery_mode()
+%% @doc Return the delivery mode for Message.
+-spec(get_delivery_mode(message()) -> delivery_mode()).
 get_delivery_mode(Message) when ?is_message(Message) ->
     (Message#content.properties)#'P_basic'.delivery_mode.
 
+%% @spec set_delivery_mode(Message::message(), Mode::delivery_mode())
+%%        -> message()
+%% @doc Set the delivery mode for Message.
+-spec(set_delivery_mode(message(), delivery_mode()) -> message()).
 set_delivery_mode(Message = #content{properties=Props}, Mode)
   when ?is_message(Message), is_integer(Mode) ->
     Message#content{properties=Props#'P_basic'{delivery_mode=Mode}}.
 
+%% @spec get_content_type(Message::message()) -> content_type()
+%% @doc Return the content type for Message.
+-spec(get_content_type(message()) -> content_type()).
 get_content_type(Message) when ?is_message(Message) ->
     (Message#content.properties)#'P_basic'.content_type.
 
+%% @spec set_content_type(Message::message(), Type::content_type()) -> message()
+%% @doc Set the content type for Message to Type.
+-spec(set_content_type(message(), content_type()) -> message()).
 set_content_type(Message = #content{properties=Props}, Type)
   when ?is_message(Message), is_binary(Type) ->
     Message#content{properties=Props#'P_basic'{content_type=Type}}.
@@ -47,15 +109,30 @@ set_content_type(Message = #content{properties=Props}, Type)
 %% Exchange helpers
 %%
 
+%% @spec new_exchange(Name::servobj_name()) -> exchange()
+%% @doc Create a new exchange definition.  Exchange type defaults to "direct".
+%% @equiv new_exchange(Name, <<"direct">>)
+-spec(new_exchange(servobj_name()) -> exchange()).
 new_exchange(Name) ->
     new_exchange(Name, <<"direct">>).
 
+%% @spec new_exchange(Name::servobj_name, Type::exchange_type()) -> exchange()
+%% @doc Create a new exchange definition of type Type.
+-spec(new_exchange(servobj_name(), exchange_type()) -> exchange()).
 new_exchange(Name, Type) when is_binary(Name), is_binary(Type) ->
     #'exchange.declare'{exchange=Name, type=Type}.
 
+%% XXX maybe call these  [get|set]_exchange_type() ?
+
+%% @spec get_type(Exchange::exchange()) -> exchange_type()
+%% @doc  Return the exchange type for Exchange. 
+-spec(get_type(exchange()) -> exchange_type()).
 get_type(#'exchange.declare'{type=Type}) ->
     Type.
 
+%% @spec set_type(Exchange::exchange(), Type::exchange_type()) -> exchange()
+%% @doc  Set the exchange type for Exchange
+-spec(set_type(exchange(), exchange_type()) -> exchange()).
 set_type(Exchange, Type) when ?is_exchange(Exchange), is_binary(Type) ->
     Exchange#'exchange.declare'{type=Type}.
 
@@ -63,7 +140,11 @@ set_type(Exchange, Type) when ?is_exchange(Exchange), is_binary(Type) ->
 %%
 %% Queue helpers
 %%
+%% 
 
+%% @spec new_queue(Name::servobj_name()) -> bunny_queue()
+%% @doc  Create a new Queue named Name
+-spec(new_queue(servobj_name()) -> bunny_queue()).
 new_queue(Name) when is_binary(Name) ->
     #'queue.declare'{queue=Name}.
 
@@ -71,6 +152,12 @@ new_queue(Name) when is_binary(Name) ->
 %% Binding helpers
 %%
 
+%% @spec new_binding(ExchangeName::servobj_name(),
+%%                   QueueName::servobj_name(),
+%%                   RoutingKey::servobj_name()) -> binding()
+%% @doc Create a new binding that routes messages delivered to Exchange 
+%%       that match RoutingKey to the queue named QueueName.
+-spec(new_binding(servobj_name(), servobj_name(), binary()) -> binding()).
 new_binding(ExchangeName, QueueName, RoutingKey)
   when is_binary(ExchangeName),
        is_binary(QueueName),
@@ -84,13 +171,16 @@ new_binding(ExchangeName, QueueName, RoutingKey)
 %% XXX: I don't particularly like this, but I don't like them having longer
 %%      names either.
 
-
+%% @spec is_durable(QueueOrExchange::durable_obj()) -> boolean()
+%% @doc  Return the durability flag on a Queue or exchange
+-spec(is_durable(durable_obj()) -> boolean()).
 is_durable(#'exchange.declare'{durable=Durable}) when is_boolean(Durable) ->
     Durable;
 is_durable(#'queue.declare'{durable=Durable}) ->
     Durable.
 
-
+%% @spec set_durable(QueueOrExchange::durable_obj(), boolean()) -> durable_obj()
+%% @doc  Set the durability flag on a Queue or Exchange
 set_durable(Exchange, Durable) when is_boolean(Durable) ->
     Exchange#'exchange.declare'{durable=Durable};
 set_durable(Queue, Durable) when ?is_queue(Queue), is_boolean(Durable) ->
