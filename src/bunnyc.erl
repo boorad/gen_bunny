8832c7ee77cf43466194ec04a0acd0057dcb25fe
David Reid <dreid@dreid.org>
2009-09-28 13:42:11 -0700

    get/ack support for bunnyc and tests for all.

--- src/bunnyc.erl
+++ src/bunnyc.erl
@@ -27,7 +27,7 @@
 
 -include_lib("gen_bunny.hrl").
 
--export([start_link/4]).
+-export([start_link/4, stop/1]).
 -export([publish/3, get/2, ack/2]).
 
 -export([init/1,
@@ -57,14 +57,24 @@ ack(Name, Tag) ->
 start_link(Name, ConnectionInfo, DeclareInfo, Args) ->
     gen_server:start_link({local, Name}, ?MODULE, [ConnectionInfo, DeclareInfo, Args], []).
 
+
+stop(Name) ->
+    gen_server:call(Name, stop).
+
+
 %%
 %% Callbacks
 %%
 
-init([ConnectionInfo, DeclareInfo, _Args]) ->
-    {ConnectionPid, ChannelPid} = bunny_util:connect(ConnectionInfo),
+init([ConnectionInfo, DeclareInfo, Args]) ->
+    ConnectFun = proplists:get_value(connect_fun, Args,
+                                     fun bunny_util:connect/1),
+    DeclareFun = proplists:get_value(declare_fun, Args,
+                                     fun bunny_util:declare/2),
 
-    {Exchange, Queue} = bunny_util:declare(ChannelPid, DeclareInfo),
+    {ConnectionPid, ChannelPid} = ConnectFun(ConnectionInfo),
+
+    {Exchange, Queue} = DeclareFun(ChannelPid, DeclareInfo),
 
     {ok, #state{connection=ConnectionPid,
                 channel=ChannelPid,
@@ -87,8 +97,11 @@ handle_call({ack, Tag}, _From, State = #state{channel=Channel}) ->
     Resp = internal_ack(Channel, Tag),
     {reply, Resp, State};
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+handle_call(stop, _From,
+            State = #state{channel=Channel, connection=Connection}) ->
+    lib_amqp:close_channel(Channel),
+    lib_amqp:close_connection(Connection),
+    {stop, normal, ok, State}.
 
 
 handle_cast(_Request, State) ->
@@ -115,8 +128,141 @@ internal_publish(Channel, Exchange, Key, Payload) ->
 
 
 internal_get(Channel, Queue, NoAck) ->
-    lib_amqp:get(Channel, Queue, NoAck).
+    lib_amqp:get(Channel, bunny_util:get_name(Queue), NoAck).
 
 
 internal_ack(Channel, DeliveryTag) ->
     lib_amqp:ack(Channel, DeliveryTag).
+
+%%
+%% Tests
+%%
+
+-include_lib("eunit/include/eunit.hrl").
+
+bunnyc_setup() ->
+    {ok, _} = mock:mock(lib_amqp),
+    ok.
+
+
+bunnyc_stop(_) ->
+    bunnyc:stop(bunnyc_test),
+
+    mock:verify_and_stop(lib_amqp),
+    ok.
+
+
+connect_and_declare_expects(TestName) ->
+    [{connect_fun,
+      fun(direct) ->
+              {dummy_conn, dummy_channel}
+      end},
+
+     {declare_fun,
+      fun(dummy_channel, N) when N =:= TestName ->
+              {#'exchange.declare'{exchange = TestName},
+               #'queue.declare'{queue = TestName}}
+      end}].
+
+
+stop_expects() ->
+    mock:expects(lib_amqp, close_channel,
+                 fun({dummy_channel}) ->
+                         true
+                 end,
+                 ok),
+
+    mock:expects(lib_amqp, close_connection,
+                 fun({dummy_conn}) ->
+                         true
+                 end,
+                 ok),
+    ok.
+
+
+bunnyc_test_() ->
+    {setup, fun bunnyc_setup/0, fun bunnyc_stop/1,
+     ?_test(
+        [begin
+             DummyFuns = connect_and_declare_expects(<<"bunnyc.test">>),
+             stop_expects(),
+             {ok, _} = bunnyc:start_link(bunnyc_test, direct,
+                                         <<"bunnyc.test">>, DummyFuns)
+         end])}.
+
+
+
+normal_setup() ->
+    {ok, _} = mock:mock(lib_amqp),
+    {ok, _} = bunnyc:start_link(
+                bunnyc_test, direct, <<"bunnyc.test">>,
+                connect_and_declare_expects(<<"bunnyc.test">>)),
+    stop_expects(),
+    ok.
+
+
+normal_stop(_) ->
+    bunnyc:stop(bunnyc_test),
+    mock:verify_and_stop(lib_amqp),
+    ok.
+
+
+publish_test_() ->
+    {setup, fun normal_setup/0, fun normal_stop/1,
+     ?_test(
+        [begin
+             mock:expects(lib_amqp, publish,
+                          fun({dummy_channel, <<"bunnyc.test">>,
+                               <<"bunnyc.test">>, <<"HELLO GOODBYE">>}) ->
+                                  true
+                          end,
+                          ok),
+
+             ?assertEqual(ok, bunnyc:publish(
+                                bunnyc_test,
+                                <<"bunnyc.test">>,
+                                <<"HELLO GOODBYE">>))
+         end])}.
+
+
+get_test_() ->
+    {setup, fun normal_setup/0, fun normal_stop/1,
+     ?_test(
+        [begin
+             mock:expects(lib_amqp, get,
+                          fun({dummy_channel, <<"bunnyc.test">>, false}) ->
+                                  true
+                          end,
+                          {<<"sometag">>,
+                           bunny_util:new_message(<<"somecontent">>)}),
+             ?assertEqual({<<"sometag">>,
+                           bunny_util:new_message(<<"somecontent">>)},
+                          bunnyc:get(bunnyc_test, false))
+        end])}.
+
+
+get_noack_test_() ->
+    {setup, fun normal_setup/0, fun normal_stop/1,
+     ?_test(
+        [begin
+             mock:expects(lib_amqp, get,
+                          fun({dummy_channel, <<"bunnyc.test">>, true}) ->
+                                  true
+                          end,
+                          bunny_util:new_message(<<"somecontent">>)),
+             ?assertEqual(bunny_util:new_message(<<"somecontent">>),
+                          bunnyc:get(bunnyc_test, true))
+        end])}.
+
+
+ack_test_() ->
+    {setup, fun normal_setup/0, fun normal_stop/1,
+     ?_test(
+        [begin
+             mock:expects(lib_amqp, ack,
+                          fun({dummy_channel, <<"sometag">>}) ->
+                                  true
+                          end,
+                          ok),
+             ?assertEqual(ok, bunnyc:ack(bunnyc_test, <<"sometag">>))
+         end])}.
